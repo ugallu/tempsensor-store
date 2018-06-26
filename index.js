b85e2e2465ba3c07456f65d94d20492943105dab
ugallu <olah.istvan.gergely@gmail.com>
2018-06-27 01:12:04 +0200

    Add date logger feature

--- index.js
+++ index.js
@@ -2,7 +2,19 @@ const express = require('express')
 const app = express()
 var port = process.env.PORT || 8080;
 
+var logList = [];
 
-app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log('Example app listening on port 80!'))
+app.get('/', (req, res) => 
+{
+    var date = new Date();    
+    logList.push(date.toISOString());
+    var html = "";
+    for(var v in logList){
+        html+="<br>"+logList[v];
+    }
+
+    res.send(html)
+})
+
+app.listen(port, () => console.log('Example app listening on port ' + port))
