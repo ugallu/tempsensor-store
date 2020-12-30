0961349082dd70cf159d368ba7607b4566999304
Oláh István Gergely <olah.gergely@united-consult.hu>
2020-12-30 17:41:45 +0100

    Add temp endpoints

--- index.js
+++ index.js
@@ -1,20 +1,76 @@
-const express = require('express')
-const app = express()
-var port = process.env.PORT || 8080;
+const express = require("express");
+const app = express();
+var bodyParser = require("body-parser");
+const sqlite3 = require("sqlite3").verbose();
 
-var logList = [];
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 
+app.use(bodyParser.json());
+var port = process.env.PORT || 7000;
 
-app.get('/', (req, res) => 
-{
-    var date = new Date();    
-    logList.push(date.toISOString());
-    var html = "";
-    for(var v in logList){
-        html+="<br>"+logList[v];
+let db = new sqlite3.Database("./temps.db", (err) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log("Connected to the temp database.");
+});
+
+db.run(
+  "CREATE TABLE IF NOT EXISTS temp(epoch integer,datetime text,temp real)"
+);
+
+db.close();
+
+app.post("/temp", (req, res) => {
+  temperature = req.body.temp;
+  date = new Date();
+  epoch = date.getTime();
+  time = date.toUTCString();
+
+  let db = new sqlite3.Database("./temps.db", (err) => {
+    if (err) {
+      console.error(err.message);
     }
+    console.log("Connected to the temp database.");
+  });
 
-    res.send(html)
-})
+  // insert one row into the langs table
+  db.run(
+    `INSERT INTO temp(epoch, datetime, temp) VALUES(?,?,?)`,
+    [epoch, time, temperature],
+    function (err) {
+      if (err) {
+        return console.log(err.message);
+      }
+      // get the last insert id
+      console.log(
+        `A row has been inserted with rowid ${this.lastID} and value ${temperature}`
+      );
+    }
+  );
+  db.close();
 
-app.listen(port, () => console.log('Example app listening on port ' + port))
+  res.sendStatus(200);
+});
+
+app.get("/temp", (req, res) => {
+  let db = new sqlite3.Database("./temps.db", (err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log("Connected to the temp database.");
+  });
+
+  // insert one row into the langs table
+  db.all(`SELECT * from temp`, function (err, rows) {
+    if (err) {
+      return console.log(err.message);
+      res.sendStatus(500);
+    }
+    res.json(JSON.stringify(rows));
+  });
+  db.close();
+});
+
+app.listen(port, () => console.log("Example app listening on port " + port));
